cf70f75856a4ec5671fa426938a19f53a71c4cec
Boska40 <stephaneboska@gmail.com>
2023-11-02 12:26:20 +0100

    ok

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Embed Google Map into Web Page</title>
+</head>
+<body>
+    <div class="mapBox">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15919.583837608076!2d9.712121349999999!3d4.0416493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2scm!4v1694947557198!5m2!1sen!2scm" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+    </div>
+</body>
+</html>
